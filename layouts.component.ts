d107cac736ce899c090fb133bbe55a2211583df5
adji0312 <99668380+adji0312@users.noreply.github.com>
2022-11-18 10:46:11 +0700

    Add files via upload

---
+++ layouts.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layouts',
+  templateUrl: './layouts.component.html',
+  styleUrls: ['./layouts.component.css']
+})
+export class LayoutsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // <script>
+  //   $(".sidebar ul li").on('click', function(){
+  //     $(".sidebar ul li.active").removeClass('active');
+  //     $(this).addClass('active')
+  //   })
+  // </script>
+  changeActive(){
+    
+  }
+
+}
